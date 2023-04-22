440a062548bacb89773b712902144c95c291583c
varmagmv@gmail.com <varmagmv@gmail.com>
2023-04-22 07:18:57 +0000

    topics.csv dbt snapshot dbt tests

---
+++ dbt_topics.sql
@@ -0,0 +1,15 @@
+-- DBT Snapshot  -- invalid hard delete and check strategy 
+-- DBT Incremental model  -- 2 exampless
+
+------------------- DBT core
+
+models -- 2 examples 4 models
+Test -- 2 example for each test type (template -- macros)
+Freshens -- examples -- change source data (2 days)
+dbt snapshot --invalid hard delete and check strategy  timestaamp
+DBT seed -- limitation 
+configs -- tags, source yml , project yml
+scheduling -- on cloud, for core -- linux, airflow-- docker
+DBT docs --  data governace 
+dbt packages 
+
